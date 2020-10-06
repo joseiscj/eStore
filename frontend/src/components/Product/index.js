c4ee13abc6f4d5dd83fb9f7d8b824ebea39e5a98
joseiscj <jose.ivan.junior@ccc.ufcg.edu.br>
2020-10-06 15:24:53 -0300

    conversão dos componentes de classes para react functional components

--- frontend/src/components/Product/index.js
+++ frontend/src/components/Product/index.js
@@ -1,18 +1,16 @@
 import React from "react";
 import "./styles.css";
 
-class Product extends React.Component {
+function Product(props) {
 
-    render() {
-        return (
-            <div className="produto">
-                <h3>{this.props.nome}</h3>
-                <h5>{this.props.descricao}</h5>
-                <h4>{"Preço: R$" + this.props.preco}</h4>
-            </div>
-        )
-    }
-    
+    return (
+        <div className="produto">
+            <h3>{props.nome}</h3>
+            <h5>{props.descricao}</h5>
+            <h4>{"Preço: R$" + props.preco}</h4>
+        </div>
+    )
+       
 };
 
 export default Product;
--- frontend/src/components/ProductList/index.js
+++ frontend/src/components/ProductList/index.js
@@ -1,42 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import Product from "../Product";
 import "./styles.css";
 
-class ProductList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            quantidadeProdutos: 0,
-            data : [{"nome": "Carregador de celular", "descricao": "Ótimo carregador de celular. Preço camarada.", preco: 50},
-                     {"nome": "Aspirador de pó", "descricao": "A forma mais eficiente de sumir com a poeira.", preco: 150},
-                     {"nome": "Playstation 5", "descricao": "A qualidade da Sony nesse console de última geração.", preco:4000}]
-        };
+function ProductList(props) {
+    const [count, setCount] = useState(0);
+    const [data, setData] = useState([{"nome": "Carregador de celular", "descricao": "Ótimo carregador de celular. Preço camarada.", preco: 50},
+    {"nome": "Aspirador de pó", "descricao": "A forma mais eficiente de sumir com a poeira.", preco: 150},
+    {"nome": "Playstation 5", "descricao": "A qualidade da Sony nesse console de última geração.", preco:4000}]);
+        
+    return (
+        <div className="plist">
+            <h1>Tipo de produto: {props.name}</h1>
+            
+            {data.map((e) => <div id="products"><Product nome={e.nome} descricao={e.descricao} preco={e.preco}/>
+            <button onClick={() => setCount(count + 1)} class="favorite styled">Interesse em comprar</button>
+            </div>)}
+            
+
+    <div id="qnt">Quantidade de produtos a serem comprados: {count}</div>
+    </div>
+    ) 
     
-        // This binding is necessary to make `this` work in the callback
-        this.incrementaQuantidade = this.incrementaQuantidade.bind(this);
-      }
-
-      incrementaQuantidade = () => {
-        this.setState(prevState => {
-           return {quantidadeProdutos: prevState.quantidadeProdutos + 1}
-        })
-    }
-    
-
-    render() {
-        return (
-            <div className="plist">
-                <h1>Tipo de produto: {this.props.name}</h1>
-                
-                {this.state.data.map((e) => <div id="products"><Product nome={e.nome} descricao={e.descricao} preco={e.preco}/>
-                <button onClick={this.incrementaQuantidade} class="favorite styled">Interesse em comprar</button>
-                </div>)}
-                
-
-        <div id="qnt">Quantidade de produtos a serem comprados: {this.state.quantidadeProdutos}</div>
-        </div>
-        ) 
-    }
 }
 
 export default ProductList;
