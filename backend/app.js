17ede61cf7e7203de4d6daa6b59960aca0b23cbc
joseiscj <jose.ivan.junior@ccc.ufcg.edu.br>
2020-10-07 15:41:56 -0300

    Adicionando o cors e definindo estado de um componente com um GET

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 
 const rotaProdutos = require("./src/routes/produtos")
 
 const rotaPedidos = require("./src/routes/pedidos")
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));  // apenas dados simples
 app.use(bodyParser.json()); // json de entrada no body
 
 app.use("/produtos", rotaProdutos);
 app.use("/pedidos", rotaPedidos);
 
+  
 app.use((req, res, next) => {
     const erro = new Error('Não encontrados');
     erro.status = 404;
--- frontend/src/components/ProductList/index.js
+++ frontend/src/components/ProductList/index.js
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import Product from "../Product";
 import "./styles.css";
+import api from '../../services/api';
 
 function ProductList(props) {
     const [count, setCount] = useState(0);
-    const [data, setData] = useState([{"nome": "Carregador de celular", "descricao": "Ótimo carregador de celular. Preço camarada.", preco: 50},
-    {"nome": "Aspirador de pó", "descricao": "A forma mais eficiente de sumir com a poeira.", preco: 150},
-    {"nome": "Playstation 5", "descricao": "A qualidade da Sony nesse console de última geração.", preco:4000}]);
+    const [data, setData] = useState([]);
+    const [page, setPage] = useState(1);
+
+    //[{"nome": "Carregador de celular", "descricao": "Ótimo carregador de celular. Preço camarada.", preco: 50},
+    //{"nome": "Aspirador de pó", "descricao": "A forma mais eficiente de sumir com a poeira.", preco: 150},
+    //{"nome": "Playstation 5", "descricao": "A qualidade da Sony nesse console de última geração.", preco:4000}]
         
+    useEffect(() => {
+        let getProdutos = async (page = 1) => {
+            let response = await api
+            .get(`/produtos?page=${page}`)
+              .catch((error) => console.log(error))
+            setData(response.data.docs);
+          };
+          console.log(data);
+          getProdutos();
+    }, [page])
+
     return (
         <div className="plist">
             <h1>Tipo de produto: {props.name}</h1>
